5371256be638a803de3eadd7e4fb68b71e3a106d
Max <bilanych.max98009@gmail.com>
2021-07-15 17:35:20 +0300

    add new moduls tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,13 @@
 import './slider';
 
 import modals from './modules/modals';
+import tabs from './modules/tabs';
 
 window.addEventListener('load', () => {
 
   modals();
+  tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+  tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 
 
 });
